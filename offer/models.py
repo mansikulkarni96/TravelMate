d935f46f2600ce75b4f9f84d8eb8693e5783cf37
Rahul Pyne <rahulpyne@ccs.neu.edu>
2018-03-17 13:27:55 -0400

    Databases Created

--- offer/models.py
+++ offer/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from travel.models import User
 
-# Create your models here.
+class Enlist(models.Model):
+    uid = models.ForeignKey(User, on_delete = models.CASCADE)
+    car_model = models.CharField(max_length = 250)
+    car_no = models.IntegerField()
+    seat = models.IntegerField()
+    from_loc = models.CharField(max_length = 250)
+    to_loc = models.CharField(max_length = 250)
+    fare = models.FloatField(default=5.0)
+    start_time = models.TimeField()
+    return_time = models.TimeField()
+    start_date = models.DateField()
--- travel/models.py
+++ travel/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    fname = models.CharField(max_length = 250)
+    lname = models.CharField(max_length = 250)
+    phone = models.IntegerField()
+    email = models.CharField(max_length = 250)
+    password = models.CharField(max_length = 250)
+    rating = models.FloatField(default=5.0)
