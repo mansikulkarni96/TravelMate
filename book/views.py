bb708b0d1e3a7f31f82185f719d339ed2e6bdeee
Tejas Parab <tejasparab890@gmail.com>
2018-03-18 23:27:10 -0400

    fixed myrides

--- book/views.py
+++ book/views.py
@@ -4,7 +4,11 @@ from django.template import loader
 from .models import Reservation
 from .models import Enlist
 
+# get form data here and
 def index(request):
+    # fromlocation = request.POST['fromlocation']
+    # tolocation = request.POST['tolocation']
+
     return render(request,'book/bookindex.html')
 
 def searchres(request):
@@ -14,6 +18,12 @@ def searchres(request):
     }
     return render(request,'book/search.html', context)
 
+# def get(self, request):
+#     form = IndexForm()
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'book/bookindex.html', context)
 
 def bookdetails(request):
     return render(request,'book/bookdetails.html')
